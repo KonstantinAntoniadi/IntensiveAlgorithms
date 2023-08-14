5f72ab5ba00dd5328356846a22c7a69c2286c59d
Edwin Evia <edwinevi@student.21-school.ru>
2023-08-14 19:20:46 +0300

    insertion_sort

--- InsertionSort/insertion_sort.hpp
+++ InsertionSort/insertion_sort.hpp
@@ -13,5 +13,35 @@ struct ListNode {
 
 class Solution {
 public:
-  ListNode *insertionSortList(ListNode *head) {}
+  ListNode *insertionSortList(ListNode *head) {
+    if (!head) {
+      return head;
+    }
+
+    std::vector<int> temp;
+
+    while (head) {
+      temp.push_back(head->val);
+      head = head->next;
+    }
+
+    for (int i = 0; i < (int)temp.size(); i++) {
+      size_t temp_index = i;
+      for (int j = i; j >= 0; j--) {
+        if (temp[temp_index] < temp[j]) {
+          std::swap(temp[temp_index], temp[j]);
+          temp_index = j;
+        }
+      }
+    }
+
+    ListNode *res = new ListNode(temp[0]);
+    ListNode *head_res = res;
+    for (size_t i = 1; i < temp.size(); i++) {
+      head_res->next = new ListNode(temp[i]);
+      head_res = head_res->next;
+    }
+
+    return res;
+  }
 };

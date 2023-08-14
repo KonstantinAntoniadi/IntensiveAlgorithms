37c3f884686cf1259f964fede9e12aaee66b3e4b
Edwin Evia <edwinevi@student.21-school.ru>
2023-08-14 18:59:12 +0300

    selection sort: format

--- SelectionSort/selection_sort.hpp
+++ SelectionSort/selection_sort.hpp
@@ -1,27 +1,27 @@
 #pragma once
 
-#include <vector>
 #include <algorithm>
+#include <vector>
 
 // https://leetcode.com/problems/sort-an-array/
 class Solution {
 public:
-  size_t FindMinIndex(std::vector<int>& nums, size_t i) {
-      size_t min = i;
-      for (size_t j = i; j < nums.size(); j++) {
-        if (nums[j] < nums[min]) {
-          min = j;
-        }
+  size_t FindMinIndex(std::vector<int> &nums, size_t i) {
+    size_t min = i;
+    for (size_t j = i; j < nums.size(); j++) {
+      if (nums[j] < nums[min]) {
+        min = j;
       }
-
-      return min;
-    }  
-    vector<int> sortArray(vector<int>& nums) {
-      std::vector<int> res = nums;
-      for (size_t i = 0; i < res.size(); i++) {
-        std::swap(res[i], res[FindMinIndex(res, i)]);
-      }
-
-      return res;
     }
+
+    return min;
+  }
+  vector<int> sortArray(vector<int> &nums) {
+    std::vector<int> res = nums;
+    for (size_t i = 0; i < res.size(); i++) {
+      std::swap(res[i], res[FindMinIndex(res, i)]);
+    }
+
+    return res;
+  }
 };
